78842c21a3a508ecf72e7918bc52a422e66f210d
Guernion Laureline <guernion.laureline@gmail.com>
2021-05-03 10:38:28 +0200

    Correction app.js

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 // Fournir des utilitaires pour travailler avec les chemins de fichiers et de répertoires
 const path = require('path');
-// Au lieu de spécifier manuellement le cors le package cors peut-être utilisé à la place
-//const cors = require('cors');
 
 //////////////////// SÉCURITÉ ////////////////////
 // Journalisation des requêtes
 const morgan = require('morgan');
 // Analyser l'activité de l'application - détecter les problèmes
 const logger = require('./config/logger');
-// Protection cookie
+// Protection cookies
 const session = require('cookie-session');
 // Sécuriser Express en définissant divers en-têtes HTTP
 const helmet = require('helmet');
@@ -26,7 +24,6 @@ const xss = require('xss-clean');
 // Limiter les demandes répétées de requêtes
 const rateLimit = require('express-rate-limit');
 
-
 //////////////////// IMPORTATION ROUTES ////////////////////
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
@@ -37,19 +34,16 @@ const app = express();
 app.use(express.json ({limite: '10kb'}));
 app.use(helmet());
 app.use(nocache());
-//app.use(cors());
 app.use(mongoSanitize());
 app.use(xss());
 app.use(morgan('combined', { stream: logger.stream }));
-
 
 //////////////////// CONNEXION À MONGODB ////////////////////
 mongoose.connect(
   "mongodb+srv://" + process.env.DB_USER_PASS + "@clusterpekocko.kqgeq.mongodb.net/databasePekocko?retryWrites=true&w=majority",
   { useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false })
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
@@ -65,7 +59,7 @@ app.use((req, res, next) => {
 let expiryDate = new Date( Date.now() + 60 * 60 * 1000 ); // expiration dans 1 heure
 app.use(session({
   name: 'session',
-  keys: [process.env.COOKIESECRET], // .env pour voir la clé secrète
+  keys: [process.env.COOKIESECRET],
   cookie: { secure: false, // Si HTTPS mettre " True "
             httpOnly: true, // booléen indiquant si le cookie doit être envoyé uniquement via HTTP(S)
             domain: 'http://localhost:3000', // Domaine du cookie
@@ -77,8 +71,8 @@ app.use(session({
 
 //////////////////// LIMITE LES REQUÊTES ////////////////////
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limiter chaque IP à 100 requêtes
+  windowMs: 24 * 60 * 60 * 1000, // 24h
+  max: 100 // limiter chaque IP à 100 requêtes
 });
 // Fonctionne sur toutes les requêtes
 app.use(limiter);
