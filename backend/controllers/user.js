c48db88845a5bb0cfc5736eb0d9343e40d008aa2
Guernion Laureline <guernion.laureline@gmail.com>
2021-05-06 14:34:07 +0200

    Ajout de maskData, csuppression de validator pour ajout de mongoose-unique-validator

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,23 +1,38 @@
+// Crypte le mote de passe
 const bcrypt = require('bcrypt');
 // Attribuer un token à l'utilisateur et permet de les vérifier
 const jwt = require('jsonwebtoken');
+// Masque l'email
+const maskData = require("../node_modules/maskdata");
+
 const User = require('../models/user');
 
+
+// Masquage de l'email avec ****@****
+const emailMask2Options = {
+  maskWith: "*",
+  unmaskedStartCharactersBeforeAt: 0,
+  unmaskedEndCharactersAfterAt: 0,
+  maskAtTheRate: false
+};
+
 //////////////////// INSCRIPTION ////////////////////
-// Crypter avec Bcrypt le password
 exports.signup = (req, res, next) => {
   // Sécuriser les champs
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
+
   const email = req.body.email;
   const password = req.body.password;
  
   if(email.match(emailRegex) && password.match(passwordRegex)) {
-    // Hasher le mot de passe de utilisateur, salé 10x
-    bcrypt.hash(req.body.password, 10)
+    // Crypter le mot de passe, sale 10x pour renforcer le cryptage
+    bcrypt.hash(password, 10)
       .then(hash => {
         const user = new User({
-          email: req.body.email,
+          // Masquer l'email
+          email: maskData.maskEmail2(email, emailMask2Options),
+          // Hash le mot de passe
           password: hash
         });
         user.save()
@@ -27,24 +42,24 @@ exports.signup = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
   } else {
       throw new Error("Mot de passe non sécurisé (ajoutez au moins un chiffre, un signe spéciaux, une lettre minuscule et majuscule) ou email invalide.");
-    }
+   }
 };
 
 //////////////////// CONNEXION ////////////////////
 exports.login = (req, res, next) => {
   // Trouver l'utilisateur correspondant à l'email
-    User.findOne({ email: req.body.email })
+    User.findOne({ email: maskData.maskEmail2(req.body.email, emailMask2Options) })
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
-        // comparer le password et le hash
+        // Comparer le mot de passe et le hash
         bcrypt.compare(req.body.password, user.password)
           .then(valid => { 
-            if (!valid) { // si false comparaison pas bonne
+            if (!valid) {
+              // si c'est false alors la comparaison n'est pas bonne
               return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
-            // Renvoi un token au front-end
             res.status(200).json({
               userId: user._id,
               // Vérifier le token a chaque requête par le front-end
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-// Appeler isEmail de la bibliothèque Validator - contrôle les emails
-const { isEmail } = require('validator');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
     // Trim permet de supprimer les espaces superflus
-    email: { type: String, required: true, validate: [isEmail], unique: true, lowercase: true, trim: true },
-    password: { type: String, required: true, max: 1024, minLength: 6, trim: true }
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true, minLength: 6, trim: true }
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
